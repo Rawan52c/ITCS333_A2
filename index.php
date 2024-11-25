58c9b2bebff18f383428c8b4bb7cc5fc6fc01f20
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-25 23:53:53 +0300

    Initial

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $URL = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
 
-$response = file_get_contents($Endpoint_URL);
+$response = file_get_contents($URL);
 
 if ($response === false) {
     die('Something went wrong, Please try again.');
