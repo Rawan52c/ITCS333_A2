91f282111089fbf6a6ab2ccae325f01d556c3c8b
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-29 20:41:21 +0300

    Update

--- index.php
+++ index.php
@@ -1,72 +1,78 @@
 <?php
 
-$URL = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
+$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
-$response = file_get_contents($URL);
+$response = file_get_contents($url);
 
-print_r($response);
+$data_output = json_decode($response, true);
 
-$output = json_decode($response, true);
-
-print_r($output);
-
-if (!$output || !isset($output ["results"])) {
+if (!$data_output || !isset($data_output["results"])) {
     die("Something went wrong, Output (data) not found");
-
 }
 
-$output_result = $output ["results"];
+$output = $data_output["results"];
 
 ?> 
 
-<!DOCTYPE html>
 <html lang="en">
 <head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UOB Student Nationality Data</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
     <link rel="stylesheet" href="css/pico.min.css">
+    <style>
+        
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
+        table th, table td {
+            padding: 10px; 
+            text-align: left;
+            border: 1px solid #ddd; 
+        }
+
+        table th {
+            background-color: #f2f2f2; 
+        }
+
+        table tbody tr:nth-child(even) {
+            background-color: #f9f9f9; 
+        }
+    </style>
 </head>
 
 <body>
-     <div class="over-flow-auto"> 
-         <table class="table table-bordered table-striped">
-            <thead>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th> Year </th>
+                <th> Semester </th>
+                <th> The Programs </th>
+                <th> Nationality </th>
+                <th> Colleges </th>
+                <th> Number of students </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            foreach ($output as $student) {
+                ?>
                 <tr>
-                    <th>Year</th>
-                    <th>Semester</th>
-                    <th>The Programs</th>
-                    <th>Nationality</th>
-                    <th>Colleges</th>
-                    <th>Number of students</th>
+                    <td><?php echo $student["year"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["semester"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["the_programs"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["nationality"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["colleges"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["number_of_students"] ?? 'N/A'; ?></td>
                 </tr>
-            </thead>
-            <tbody>
-
-                <?php
-           
-                foreach ($output_result as $student_data) {
-                ?>
-                    <tr> 
-                         <td> <?php echo $student_data["Year"]; ?> </td>
-                         <td> <?php echo $student_data["Semester"]; ?> </td>
-                         <td> <?php echo $student_data["The_Programs"]; ?> </td>
-                         <td> <?php echo $student_data["Nationality"]; ?> </td>
-                         <td> <?php echo $student_data["Colleges"]; ?> </td>
-                         <td> <?php echo $student_data["Number_of_students"] ?? 'N/A'; ?> </td>
-                    </tr> 
-            
-                <?php 
-
-                }
-                ?>
-            </tbody> 
-
- 
-        </div>
- 
+            <?php 
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
 
