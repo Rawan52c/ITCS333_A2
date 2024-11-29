6178c75eb768fe22db2e1c7c3c4eb04b1842cbff
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-29 19:53:52 +0300

    Update

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ $output_result = $output ["results"];
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
     <title>UOB Student Nationality Data</title>
     <link rel="stylesheet" href="css/pico.min.css">
 
