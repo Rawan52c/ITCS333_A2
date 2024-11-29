e14b040be5186d5fe48875abdbefee5189a8ae36
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-29 18:41:50 +0300

    Update

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 
 $URL = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
+
 $response = file_get_contents($URL);
 
-if ($response === false) {
-    die('Something went wrong, Please try again.');
+print_r($response);
+
+$output = json_decode($response, true);
+
+print_r($output);
+
+if (!$output || !isset($output ["results"])) {
+    die("Something went wrong, Output (data) not found");
+
 }
 
-$result = json_decode($response, true);
+$output_result = $output ["results"];
+
 ?> 
 
 <!DOCTYPE html>
@@ -17,27 +26,49 @@ $result = json_decode($response, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>UOB student nationality data</title>
+    <title>UOB Student Nationality Data</title>
     <link rel="stylesheet" href="css/pico.min.css">
 
 </head>
 
 <body>
-<div class="container">
-    <h1>University of Bahrain Students Enrollment by Nationality</h1>
+     <div class="over-flow-auto"> 
+         <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Semester</th>
+                    <th>The Programs</th>
+                    <th>Nationality</th>
+                    <th>Colleges</th>
+                    <th>Number of students</th>
+                </tr>
+            </thead>
+            <tbody>
 
-    if (isset($result['results']) && is_array($result['results'])) {
+                <?php
+           
+                foreach ($output_result as $student_data) {
+                ?>
+                    <tr> 
+                         <td> <?php echo $student_data["Year"]; ?> </td>
+                         <td> <?php echo $student_data["Semester"]; ?> </td>
+                         <td> <?php echo $student_data["The_Programs"]; ?> </td>
+                         <td> <?php echo $student_data["Nationality"]; ?> </td>
+                         <td> <?php echo $student_data["Colleges"]; ?> </td>
+                         <td> <?php echo $student_data["Number_of_students"] ?? 'N/A'; ?> </td>
+                    </tr> 
+            
+                <?php 
 
-<?php include './table.php';?>
+                }
+                ?>
+            </tbody> 
 
-} else {
-echo '<p> Error, Please Try Again.';
-}
-
-</div>
-
+ 
+        </div>
+ 
 </body>
-
 </html>
 
 
