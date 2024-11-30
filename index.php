3917ca5e6df8fd1382d73dcd2612e114b2693bb6
Aysha Abdulrahman <ayshaarahman604@gmail.com>
2024-11-30 11:56:38 +0300

    updated

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// URL of the API endpoint with query parameters to fetch data.
 $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
 // Fetch the API response using file_get_contents.
@@ -10,6 +11,8 @@ $data_output = json_decode($response, true);
 
 // Check if the response is valid and contains the 'results' key.
 if (!$data_output || !isset($data_output["results"])) {
+
+    // Terminate execution with an error message if data is missing or invalid.
     die("Something went wrong, Output (data) not found");
 }
 
