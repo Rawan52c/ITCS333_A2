f130c1dcd1097121c42be9fec83c8d74588fe05a
Aysha Abdulrahman <ayshaarahman604@gmail.com>
2024-11-30 11:46:51 +0300

    updated

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
+// Fetch the API response using file_get_contents.
 $response = file_get_contents($url);
 
+// Decode the JSON response into an associative array.
 $data_output = json_decode($response, true);
 
+// Check if the response is valid and contains the 'results' key.
 if (!$data_output || !isset($data_output["results"])) {
     die("Something went wrong, Output (data) not found");
 }
