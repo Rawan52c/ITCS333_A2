ba3d62e0f2a786681e1c5353a5d277835b19bae3
Aysha Abdulrahman <ayshaarahman604@gmail.com>
2024-11-30 12:08:36 +0300

    updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // URL of the API endpoint to fetch data.
 $url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
-// Fetch the API response using file_get_contents.
+// Fetch the API response as a JSON string using file_get_contents.
 $response = file_get_contents($url);
 
 // Decode the JSON response into an associative array.
